bb6ed9122e8be8dcc8fae003783c9c770ed0aa2b
Jake <llira09@gmail.com>
2020-07-22 20:56:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,11 @@
 def square_array(array)
   # new_array = []
-  # array.length.times do |index|
-  #   new_array.push(Math.sqrt(array[index]))
-  # end
-  # new_array
+  
 end
 
 
 
+# array.length.times do |index|
+  #   new_array.push(Math.sqrt(array[index]))
+  # end
+  # new_array
